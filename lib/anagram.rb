c8dd91f6d5dd9d6c9435ecaa75b212272398ad28
Shayla White <shayladwhite1@gmail.com>
2019-06-10 02:59:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,22 @@
-class Anagram
-  
-  attr_accessor:
-  def initialize(Anagram)
-    @Anagram = Anagram
-  end
-  
-  def self.match
-  end
-end
 
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    anagrams =[]
+    sorted_word = word.split("").sort
+    array.each do |array_word|
+      anagrams << array_word if array_word.split("").sort == sorted_word
+    end
+    anagrams
+
+  end
+
+
+end
